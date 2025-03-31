f73dace20055f277b7aa14a22b3b881bc09a9766
ayushkumar-bit <aayushjmp43@gmail.com>
2025-03-31 19:43:31 +0530

    Add files via upload

---
+++ project1/html/contactus.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<title>Contact Us|Hotel Asphodel</title>
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+		<link rel = "stylesheet" type = "text/css" href = "css/styles.css" />
+	</head>
+<body>
+	<nav style = "background-color:rgba(0, 0, 0, 0.1);" class = "navbar navbar-default">
+		<div  class = "container-fluid">
+			<div class = "navbar-header">
+				<a class = "navbar-brand" >Hotel Asphodel</a>
+			</div>
+		</div>
+	</nav>	
+	<ul id = "menu">
+		<li><a href = "index.php">Home</a></li> |
+		<li><a href = "aboutus.php">About us</a></li> |
+		<li><a href = "contactus.php">Contact us</a></li> |
+		<li><a href = "gallery.php">Gallery</a></li> |
+		<li><a href = "dineandlounge.php">Dine and Lounge</a></li> |			
+		<li><a href = "reservation.php">Make a reservation</a></li> |
+		<li><a href = "rulesandregulation.php">Rules and Regulation</a></li>
+	</ul>
+	<div style = "margin-left:0;" class = "container">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<strong><h3>CONTACT US</h3></strong>
+				<br />
+				<br />
+				<center><img src = "C:\xampp\htdocs\project1\html\pics\image4.jpg" width = "300" height = "300" /></center>
+				<br />
+				<br />
+				<center>
+				<p>Contact No: +91- 8102550042</p>
+				<p>Email: rooms@hotel.com</p>
+				</center>
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	<div style = "text-align:right; margin-right:10px;" class = "navbar navbar-default navbar-fixed-bottom">
+		
+	</div>
+</body>
+<script src = "js/jquery.js"></script>
+<script src = "js/bootstrap.js"></script>	
+</html>
